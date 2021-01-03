fixed precision added recalll

EX4/project4.py
@@ -63,7 +63,7 @@ while True:
     # object
     box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
     (startX, startY, endX, endY) = box.astype("int")
-    
+        ## na
         # draw the bounding box of the face along with the associated
         # probability
     text = "{:.2f}%".format(confidence * 100)